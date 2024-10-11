8a8ebfbce16b7c51d253614f2cc5c469f75b07cc
Shahriyor <xudayorov1399@gmail.com>
2024-10-11 15:14:54 +0500

    inisuall setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button"
+
+function App() {
+  
+
+  return (
+    <div className="w-full h-screen flex justify-center items-center">
+      <Button variant="outline">click</Button>
+    </div>
+  )
+}
+
+export default App
