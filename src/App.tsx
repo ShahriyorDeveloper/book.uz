9c3c6aadbf59d1fef79aaecc1a12a27e4ca4b20b
Shahriyor <xudayorov1399@gmail.com>
2024-10-11 15:51:46 +0500

    feat: added husky hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 
 function App() {
-  
-
-  return (
-    <div className="w-full h-screen flex justify-center items-center">
-      <Button variant="outline">click</Button>
-    </div>
-  )
+	return (
+		<div className="w-full h-screen flex justify-center items-center">
+			<Button variant="outline">click</Button>
+		</div>
+	);
 }
 
-export default App
+export default App;
